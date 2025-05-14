cd7dc36b9a42bafd61d7474a9eec9c999e8561e7
Aravindan22 <aravindan7822@gmail.com>
2025-05-14 20:52:13 +0530

    Test D1

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -2,16 +2,19 @@ export const config = {
   runtime: "edge", // Required to access `env`
 };
 
-export default {
-  async fetch(request, env) {
-    const result = await env.DB.prepare(
-      "SELECT value FROM SampleTable"
-    ).first();
-    return new Response(result?.value || "Not found");
-  },
-};
+// export default {
+//   async fetch(request, env) {
+//     const result = await env.DB.prepare(
+//       "SELECT value FROM SampleTable"
+//     ).first();
+//     return new Response(result?.value || "Not found");
+//   },
+// };
 // app/api/test/route.js
 export function GET() {
+  console.log("GET /api/test called");
   const result = env.DB.prepare("SELECT value FROM SampleTable").first();
+  console.log(result);
+
   return new Response(result?.value || "Not found");
 }
