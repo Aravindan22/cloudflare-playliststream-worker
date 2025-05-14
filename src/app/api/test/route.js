718d860ef79bedd09aebe3ec908bb98a0b5ccceb
Aravindan22 <aravindan7822@gmail.com>
2025-05-14 21:06:08 +0530

    Test D1 nextjs

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -2,19 +2,25 @@ export const config = {
   runtime: "edge", // Required to access `env`
 };
 
-// export default {
-//   async fetch(request, env) {
-//     const result = await env.DB.prepare(
-//       "SELECT value FROM SampleTable"
-//     ).first();
-//     return new Response(result?.value || "Not found");
-//   },
-// };
-// app/api/test/route.js
-export function GET() {
-  console.log("GET /api/test called");
-  const result = env.DB.prepare("SELECT value FROM SampleTable").first();
-  console.log(result);
+export default {
+  async fetch(request, env) {
+    const { pathname } = new URL(request.url);
 
-  return new Response(result?.value || "Not found");
-}
+    if (pathname === "/api/query") {
+      const result = env.DB.prepare("SELECT value FROM SampleTable").first();
+      console.log(result);
+      return new Response(JSON.stringify(result));
+    }
+
+    return new Response("Not found", { status: 404 });
+  },
+};
+
+// // app/api/test/route.js
+// export function GET() {
+//   console.log("GET /api/test called");
+//   const result = env.DB.prepare("SELECT value FROM SampleTable").first();
+//   console.log(result);
+
+//   return new Response(result?.value || "Not found");
+// }
